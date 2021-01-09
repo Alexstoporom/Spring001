223c78b806d38ba22b418781f6aa3f9c2837ee7b
Alexstoporom <the_prometey@mail.ru>
2021-01-09 13:40:15 +1000

    3

---
+++ src/main/java/com/example/Spring001/Application.java
@@ -0,0 +1,14 @@
+package com.example.Spring001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+int i=1;
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
